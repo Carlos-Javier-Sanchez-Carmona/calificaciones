baa830556def8e430af983ea8cca87cf0d78fdb3
Carlitos Panzer <83928588+Carlos-Javier-Sanchez-Carmona@users.noreply.github.com>
2024-01-29 07:23:33 -0600

    promedios por materia funcionando, solo falta el promedio general

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", ready);
 function ready() {
-    //alert('DOM cargado con exito');
-    console.log('DOM cargado con exito');
-};
+  //alert('DOM cargado con exito');
+  console.log("DOM cargado con exito");
+}
 
-//funcion promedio, obtencion de valores de los input
-function promedio() {
+function promedioGral() {
+  //funcion promedio, obtencion de valores de los input
+  function promedio() {
     var calificacion1_1 = document.getElementById("calificacion1_1").value;
     var calificacion1_2 = document.getElementById("calificacion1_2").value;
     var calificacion1_3 = document.getElementById("calificacion1_3").value;
-    
+
     var calificacion2_1 = document.getElementById("calificacion2_1").value;
     var calificacion2_2 = document.getElementById("calificacion2_2").value;
     var calificacion2_3 = document.getElementById("calificacion2_3").value;
@@ -17,73 +18,90 @@ function promedio() {
     var calificacion3_1 = document.getElementById("calificacion3_1").value;
     var calificacion3_2 = document.getElementById("calificacion3_2").value;
     var calificacion3_3 = document.getElementById("calificacion3_3").value;
-    
-    var promedio1 = (parseFloat(calificacion1_1) + parseFloat(calificacion1_2) + parseFloat(calificacion1_3)) / 3;
+
+    var promedio1 =
+      (parseFloat(calificacion1_1) +
+        parseFloat(calificacion1_2) +
+        parseFloat(calificacion1_3)) /
+      3;
     document.getElementById("promedio1").innerHTML = promedio1.toFixed(2);
-    
-    var promedio2 = (parseFloat(calificacion2_1) + parseFloat(calificacion2_2) + parseFloat(calificacion2_3)) / 3;
+    console.log("Promedio 1: " + promedio1);
+    var promedio2 =
+      (parseFloat(calificacion2_1) +
+        parseFloat(calificacion2_2) +
+        parseFloat(calificacion2_3)) /
+      3;
     document.getElementById("promedio2").innerHTML = promedio2.toFixed(2);
-
-    var promedio3 = (parseFloat(calificacion3_1) + parseFloat(calificacion3_2) + parseFloat(calificacion3_3)) / 3;
+    console.log("Promedio 2:" + promedio2);
+    var promedio3 =
+      (parseFloat(calificacion3_1) +
+        parseFloat(calificacion3_2) +
+        parseFloat(calificacion3_3)) /
+      3;
     document.getElementById("promedio3").innerHTML = promedio3.toFixed(2);
+    console.log("Promedio 3: " + promedio3);
+  }
 
-};
+  //invocando funcion para los promedios por materia
+  promedio();
 
 
-//invocando funcion
-promedio();
-
-//evento escucha cambio en los inputs
-calificacion1_1.addEventListener( "change", function() {
+  //evento escucha cambio en los inputs
+  calificacion1_1.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion1 ha cambiado a: ${this.value}`);
-});
+  });
 
-calificacion1_2.addEventListener( "change", function() {
+  calificacion1_2.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion2 ha cambiado a: ${this.value}`);
-});
+  });
 
-calificacion1_3.addEventListener( "change", function() {
+  calificacion1_3.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion3 ha cambiado a: ${this.value}`);
-})
+  });
 
-calificacion2_1.addEventListener( "change", function() {
+  calificacion2_1.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion1 ha cambiado a: ${this.value}`);
-});
+  });
 
-calificacion2_2.addEventListener( "change", function() {
+  calificacion2_2.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion2 ha cambiado a: ${this.value}`);
-});
+  });
 
-calificacion2_3.addEventListener( "change", function() {
+  calificacion2_3.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion3 ha cambiado a: ${this.value}`);
-})
+  });
 
-calificacion3_1.addEventListener( "change", function() {
+  calificacion3_1.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion1 ha cambiado a: ${this.value}`);
-});
+  });
 
-calificacion3_2.addEventListener( "change", function() {
+  calificacion3_2.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion2 ha cambiado a: ${this.value}`);
-});
+  });
 
-calificacion3_3.addEventListener( "change", function() {
+  calificacion3_3.addEventListener("change", function () {
     promedio();
     console.log(`el valor de calificacion3 ha cambiado a: ${this.value}`);
-});
+  });
 
-/*
-document.querySelectorAll('input[type="number"]').forEach(item => {
-    item.addEventListener('change', event => {
-        console.log("haz cambiado el valor", event.target.value);
-      promedio();
-    })
-  })
-*/
+  //get all promedio
+let promedio_materia1 = promedio
+let promedio_materia2 = promedio;
+let promedio_materia3 = promedio;
+//calculate average of averages
+const promedioGral =
+  (parseFloat(promedio_materia1)+ parseFloat(promedio_materia2) + parseFloat(promedio_materia3)) / 3;
+document.getElementById("promedio_gral").innerHTML = promedioGral.toFixed(2);
+}
+
+
+//invocando funcion para los promedios generales
+promedioGral();
